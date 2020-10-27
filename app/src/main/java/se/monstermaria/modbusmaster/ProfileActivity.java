642518c6c2f65a8cbdce28731f3c376305ab2db3
monstermaria <42701428+monstermaria@users.noreply.github.com>
2020-10-27 05:36:41 +0100

    Saving chosen profile id to preferences

--- app/src/main/java/se/monstermaria/modbusmaster/ProfileActivity.java
+++ app/src/main/java/se/monstermaria/modbusmaster/ProfileActivity.java
@@ -2,6 +2,7 @@ package se.monstermaria.modbusmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
@@ -35,10 +36,17 @@ public class ProfileActivity extends AppCompatActivity implements AdapterView.On
         profileSpinner.setOnItemSelectedListener(this);
     }
 
+    private void saveProfileToPreferences() {
+        SharedPreferences.Editor editor = getSharedPreferences("Active profile", MODE_PRIVATE).edit();
+        editor.putString("ID", String.valueOf(activeProfile.id));
+        editor.apply();
+    }
+
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         activeProfile = (Profile) parent.getItemAtPosition(position);
-        System.out.println("Profile selected: " + activeProfile.toString());
+        saveProfileToPreferences();
+        System.out.println("Profile selected: " + activeProfile.toString() + " ID: " + activeProfile.id);
         EditText name = findViewById(R.id.editTextProfileName);
         name.setText(activeProfile.name != null ? activeProfile.name : "");
         EditText ipAddress = findViewById(R.id.editTextIpAddress);
